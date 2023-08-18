a3da74b404fc6c8defd881b2e01ed5c1137e4e15
Jozimar1990 <jozimar.1990@gmail.com>
2023-08-18 18:49:37 -0400

    atualização

--- desafio26.py
+++ desafio26.py
@@ -2,6 +2,8 @@
 #A função deve aceitar dois argumentos: a base e o expoente.
 #No entanto, se o exponte não for fornecido, ele deve assumir o valor padrão de 2.
 
+
+
 def quadrado(a, b = 2):
     return   a**b
 
--- matematica.py
+++ matematica.py
@@ -1,13 +1,18 @@
+#https://pense-python.caravela.club/11-dicionarios/07-variaveis-globais.html
+
+
 def fat(a):
     res = 1
     for i in range(1,a+1):
         res *= i
     return res
-
+global fator
+fator = 1
 def fat2(a):
     b = a
     def fatorar(b):
+        global fator
         fator *= b-1
         return fator if b == 1 else fatorar(b) 
     
-    return fatorar(b)
+    return fatorar(a)
