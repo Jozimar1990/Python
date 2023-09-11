10ef3c40f989470e802ee9bcaf21916a708a1670
Jozimar1990 <jozimar.1990@gmail.com>
2023-09-11 19:05:32 -0400

    novo

---
+++ aula11.py
@@ -0,0 +1,8 @@
+#criar a classe
+class computador:
+    def __init__ (self, marca, ram, placaDeVideo):
+    self.marca = marca
+    self.ram = ram
+    self.placaDeVideo = placaDeVideo
+
+computador('Asus', '16GB', "AMD")
