3522b85ef82b411b2a0a4bd5e08b6d527eb8975c
Jozimar1990 <jozimar.1990@gmail.com>
2023-09-11 19:12:55 -0400

    novo

--- aula11.py
+++ aula11.py
@@ -4,6 +4,12 @@ class computador:
         self.marca = marca
         self.ram = ram
         self.placaDeVideo = placaDeVideo
+    
+    def exibirInformacoes(self):
+        print(self.marca, self.ram, self.placaDeVideo)
+    
+    def desligar(self):
+        print("Desligando")
 
 computador1 = computador('Asus', '16GB', "AMD")
 print(computador1.marca)
