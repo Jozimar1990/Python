27690aaf95fc6915854aec0add362b849b007bfc
Jozimar1990 <jozimar.1990@gmail.com>
2023-09-12 20:44:44 -0400

    nova atualização

--- teste2.py
+++ teste2.py
@@ -1,7 +1,10 @@
 import moeda as md
+from moeda import diminuir as dm
 preco = 180.00
 taxa = 10
 print(f'O aumento do valor {preco} é de {md.aumentar(preco, taxa)}')
-print(f'O decrescimo do valor {preco} é de {md.diminuir(preco, taxa)}')
+print(f'O decréscimo do valor {preco} é de {md.diminuir(preco, taxa)}')
 print(f'O dobro do valor {preco} é de {md.dobro(preco)}')
 print(f'A metade do valor {preco} é de {md.metade(preco)}')
+
+print(f'O decréscimo do valor {preco} é de {dm(preco, taxa)}')
