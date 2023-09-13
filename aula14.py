2b69765152bb8fe032b3472dd2016194969a63e4
Jozimar1990 <jozimar.1990@gmail.com>
2023-09-13 19:56:09 -0400

    nova

--- aula14.py
+++ aula14.py
@@ -4,9 +4,9 @@ pintar uma parede. O usuário deverá fornecer as seguintes
 informações: Rendimento, altura e largura.
 O programa deve mostrar na tela a mensagem 'Você necessita de x latas de tinta
 '''
-from Python.calculaTinta import calcular as c
+from calculaTinta import calcular as c
 rendimento = float(input("Informe o rendimento em (m²) da lata de tinta: "))
 altura = float(input("Informe a altura da parede em (m²): "))
 largura = float(input("Informe a largura da parede em (m²): "))
 
-print(f'Você necessita de {c(rendimento, altura, largura)}')
+print(f'Você necessita de {c(rendimento, altura, largura)} latas de tinta')
